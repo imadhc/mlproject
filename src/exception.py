8661a83ae910e2f0d93f57b73ed836211e65db7a
imadhc <imadhcareem009@gmail.com>
2025-01-15 23:15:11 +0530

    corrected logger and exception

--- src/exception.py
+++ src/exception.py
@@ -1,13 +1,11 @@
 import sys
-import logger
+import src.logger as logger
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in Python script name [{0}] line [{1}] message[{2}]."format(
-    file_name, exc_tb.tb_lineno, str(error)
-
-    )
+    file_name, exc_tb.tb_lineno, str(error))
     return error_message
 
 class CustomException(Exception):

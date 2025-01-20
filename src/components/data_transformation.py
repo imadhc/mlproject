b7d60abe0e98ef0afb19ffc79896c278f1fecb63
imadhc <imadhcareem009@gmail.com>
2025-01-21 03:16:52 +0530

    Model Training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -82,6 +82,21 @@ class DataTransformation:
             numerical_columns = ["writing_score", "reading_score"]
 
             input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            logging.info(f"Applying preprocessing object on training dataframe and testing dataframe")
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info(f"Saved Preprocessing Object.")
+
 
             save_object(
 
@@ -89,5 +104,12 @@ class DataTransformation:
                 obj = preprocessing_obj
             )
 
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_ob_file_path
+            )
+
         except Exception as e:
             raise CustomException(e, sys)
+        
